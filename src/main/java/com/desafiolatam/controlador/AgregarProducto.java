029e1bf0742c979462e6377668d14e7d3c7a74f9
Camilo Lavado <camilolavado@petalmail.com>
2022-03-18 20:30:24 -0300

    C:/Program Files/Git/AgregarProducto

--- src/main/java/com/desafiolatam/controlador/AgregarProducto.java
+++ src/main/java/com/desafiolatam/controlador/AgregarProducto.java
@@ -1,33 +1,68 @@
 package com.desafiolatam.controlador;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class AgregarProducto
- */
+import com.desafiolatam.DAO.CategoriaDao;
+import com.desafiolatam.DAO.CategoriaDaoImpl;
+import com.desafiolatam.DAO.ProductoDao;
+import com.desafiolatam.DAO.ProductoDaoImpl;
+import com.desafiolatam.modelo.Categoria;
+import com.desafiolatam.modelo.Producto;
+
+
 @WebServlet("/AgregarProducto")
 public class AgregarProducto extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	ProductoDao productoDao = new ProductoDaoImpl();
+	CategoriaDao categoriaDao = new CategoriaDaoImpl();
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		List<Categoria> categorias = categoriaDao.listarCategorias();
+		
+		request.setAttribute("categorias", categorias);
+	
+		request.getRequestDispatcher("AgregarProducto.jsp").forward(request, response);
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	
+		request.setCharacterEncoding("UTF-8");
+		
+		
+		
+		
+		String nombreProducto = request.getParameter("Nombre");
+		int precioProducto = Integer.parseInt(request.getParameter("precio"));
+		String descripcionProducto = request.getParameter("descripcion");
+		int idCategoria = Integer.parseInt(request.getParameter("Categoria"));
+	
+		int id = productoDao.obtenerUltimoId() + 1;
+		
+		Producto producto = new Producto ();
+		
+		producto.setIdCategoria(id);
+		producto.setNombreProducto(nombreProducto);
+		producto.setPrecioProducto(precioProducto);
+		producto.setDescripcionProducto(descripcionProducto);
+		producto.setIdCategoria(idCategoria);
+		
+		productoDao.agregarProducto(producto);
+		
+		request.getRequestDispatcher("PaginaInicio.jsp").forward(request, response);
+		
+		
 	}
 
 }
